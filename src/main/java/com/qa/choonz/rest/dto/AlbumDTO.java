cc510acfc731ca90f65390ac3cd4b0e927c56297
Sue Bluck <SBluck@academytrainee.com>
2021-01-21 19:21:44 +0000

    CMQ-36 #comment SB: add AlbumController unit testing and associated changes

--- src/main/java/com/qa/choonz/rest/dto/AlbumDTO.java
+++ src/main/java/com/qa/choonz/rest/dto/AlbumDTO.java
@@ -3,19 +3,17 @@ package com.qa.choonz.rest.dto;
 import java.util.List;
 
 import com.qa.choonz.persistence.domain.Artist;
-import com.qa.choonz.persistence.domain.Track;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @NoArgsConstructor
 public class AlbumDTO {
 
-    private long id;
-    private String name;
-    private List<Track> tracks;
-    private Artist artist;
-    
+	private long id;
+	private String name;
+	private List<TrackDTO> tracks;
+	private Artist artist;
+
 }
