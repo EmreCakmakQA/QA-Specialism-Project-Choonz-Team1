f7672a3e293d80c11153524c71d65ce75242923e
Sue Bluck <SBluck@academytrainee.com>
2021-01-22 14:31:52 +0000

    CMQ-69 #comment SB: add sql schema and data files

---
+++ src/test/resources/choonz-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO artist (`name`) VALUES ('Elvis Presley');
+INSERT INTO album (`name`) VALUES ('Vegas');
+INSERT INTO genre (`name`, `description`) VALUES ('Rock n Roll', '50s Rock n Roll');
+INSERT INTO playlist (`name`, `description`) VALUES ('Rockin', 'Rock around the Clock');
+INSERT INTO track (`name`, `description`, `duration`, `lyrics`) VALUES ('Jailhouse Rock', 'Released 1957', 155, 'The warden threw a party in the county jail');
+INSERT INTO album (`name`) VALUES ('Elvis');
